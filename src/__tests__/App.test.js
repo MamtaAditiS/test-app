3b2c2fda30d4e57bef095d8974353e10949a0fd4
MamtaAditiS <mamtasingla007@gmail.com>
2018-09-20 18:36:48 +0530

    worked on test cases

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../containers/App';
+import App from '../containers/App'
+
+import { configure, shallow } from 'enzyme';
 
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App/>);
 });
+
+console.log(wrapped , "wrapped")
+
+describe('Running test cases for App' , () => {
+  
+  it('should execute after component loads', () => {
+    expect(wrapped.find('.App').length).toEqual(1); 
+  });
+  
+  it('should execute after app div ', () => {
+    expect(wrapped.find('.app-column1').length).toEqual(1);
+  });
+  
+  it('should execute after column 1 div', () => {
+    expect(wrapped.find('.app-column2').length).toEqual(1)
+  });
+
+})
